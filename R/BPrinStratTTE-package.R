dfb6c3d3d47adc4c5e128cfe18e788ab18c58011
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2024-01-16 14:34:41 +0200

    Add missing rstantools dependency

--- R/BPrinStratTTE-package.R
+++ R/BPrinStratTTE-package.R
@@ -14,6 +14,7 @@
 #' @import methods
 #' @import Rcpp
 #' @importFrom rstan sampling
+#' @importFrom rstantools rstan_config
 #'
 #' @references
 #' Stan Development Team (2022). RStan: the R interface to Stan. R package version 2.21.5. https://mc-stan.org
