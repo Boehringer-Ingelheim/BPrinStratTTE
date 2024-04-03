e7b2eba77b764f481d149280ff5370878cd29220
Stock,Dr.,Christian (MED BDS) BIP-DE-I <christian.stock@boehringer-ingelheim.com>
2024-04-03 21:12:25 +0200

    Updates

--- R/BPrinStratTTE-package.R
+++ R/BPrinStratTTE-package.R
@@ -7,7 +7,6 @@
 #'     <doi:10.1111/j.0006-341x.2002.00021.x> and Imbens and Rubin (1997)
 #'     <doi:10.1214/aos/1034276631>, and intended to be applied to a 
 #'     specific time-to-event setting.#'
-#' @docType package
 #' @name BPrinStratTTE-package
 #' @aliases BPrinStratTTE
 #' @useDynLib BPrinStratTTE, .registration = TRUE
@@ -22,17 +21,18 @@
 NULL
 
 ## usethis namespace: start
+#' @importFrom dplyr filter
+#' @importFrom dplyr mutate
+#' @importFrom dplyr select
+#' @importFrom furrr future_map
+#' @importFrom magrittr %>%
+#' @importFrom purrr map
+#' @importFrom RcppParallel RcppParallelLibs
 #' @importFrom stats pexp
 #' @importFrom stats rexp
 #' @importFrom stats runif
-#' @importFrom dplyr select
-#' @importFrom dplyr filter
-#' @importFrom dplyr mutate
-#' @importFrom purrr map
-#' @importFrom furrr future_map
-#' @importFrom magrittr %>%
+#' @importFrom stringr str_pad
 #' @importFrom tibble as_tibble
 #' @importFrom tibble tibble
-#' @importFrom stringr str_pad
 ## usethis namespace: end
 NULL
